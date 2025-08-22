190d0e6ba5a67e86a1ac98ce5d37d80af29cd5fc
HellionZero <l3304dev@gmail.com>
2025-08-22 12:33:31 -0300

    solução temporária

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,134 @@
 /*   By: lsrtn_soft <lsrtn_soft@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 22:37:55 by lsrtn_soft        #+#    #+#             */
-/*   Updated: 2025/08/18 20:51:11 by lsrtn_soft       ###   ########.fr       */
+/*   Updated: 2025/08/20 20:18:14 by lsrtn_soft       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static char	*ft_strchr(char *s, int c)
+{
+	unsigned int	i;
+	char			cc;
+
+	cc = (char) c;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == cc)
+			return ((char *) &s[i]);
+		i++;
+	}
+	if (s[i] == cc)
+		return ((char *) &s[i]);
+	return (NULL);
+}
+
+static char	*_fill_line_buffer(int fd, char *left_c, char *buffer)
+{
+	ssize_t	b_read;
+	char	*tmp;
+
+	b_read = 1;
+	while (b_read > 0)
+	{
+		b_read = read(fd, buffer, BUFFER_SIZE);
+		if (b_read == -1)
+		{
+			free(left_c);
+			return (NULL);
+		}
+		else if (b_read == 0)
+			break ;
+		buffer[b_read] = 0;
+		if (!left_c)
+			left_c = ft_strdup("");
+		tmp = left_c;
+		left_c = ft_strjoin(tmp, buffer);
+		free(tmp);
+		tmp = NULL;
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	return (left_c);
+}
+
+static char	*_set_line(char *line_buffer)
+{
+	char	*left_c;
+	ssize_t	i;
+
+	i = 0;
+	while (line_buffer[i] != '\n' && line_buffer[i] != '\0')
+		i++;
+	if (line_buffer[i] == 0 || line_buffer[1] == 0)
+		return (NULL);
+	left_c = ft_substr(line_buffer, i + 1, ft_strlen(line_buffer) - i);
+	if (*left_c == 0)
+	{
+		free(left_c);
+		left_c = NULL;
+	}
+	line_buffer[i + 1] = 0;
+	return (left_c);
+}
+
 char	*get_next_line(int fd)
 {
-	char			*next;
+	static char	*left_c;
+	char		*line;
+	char		*buffer;
 
+	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	{
+		free(left_c);
+		free(buffer);
+		left_c = NULL;
+		buffer = NULL;
+		return (NULL);
+	}
+	if (!buffer)
+		return (NULL);
+	line = _fill_line_buffer(fd, left_c, buffer);
+	free(buffer);
+	buffer = NULL;
+	if (!line)
+		return (NULL);
+	left_c = _set_line(line);
+	return (line);
+}
+
+/*char	*get_next_line(int fd)
+{
+	static char		buf[BUFFER_SIZE];
+	char			*line;
+	ssize_t			ch_count;
+	size_t			i;
+
+	i = 0;
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
-	return (next);
-}
+	
+	line = calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (!line)
+		return (NULL);
+	ch_count = read(fd, buf, BUFFER_SIZE);
+	if (!ch_count)
+	{
+		free(line);
+		return (NULL);
+	}
+	buf[ch_count] = 0;
+	if (ch_count <= 0)
+		return (line);
+	while (buf[i])
+	{
+		if (buf[i] == '\n')
+			break ;
+		line[i] = buf[i];
+		i++;
+	}
+	return (line);
+}*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lsrtn_soft <lsrtn_soft@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 22:39:38 by lsrtn_soft        #+#    #+#             */
-/*   Updated: 2025/08/18 20:50:08 by lsrtn_soft       ###   ########.fr       */
+/*   Updated: 2025/08/20 20:01:48 by lsrtn_soft       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,17 @@
 #  define BUFFER_SIZE 42
 # endif
 
-# include<unistd.h>
-# include<stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 char	*get_next_line(int fd);
 void	*ft_calloc(size_t count, size_t size);
-
+void	*ft_memcpy(void *s1, void *s2, size_t n);
+void	*ft_realloc(void *ptr, size_t size);
+char	*ft_strdup(char *s1);
+size_t	ft_strlen(char *s);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+char	*ft_strjoin(char *s1, char *s2);
+void	fill_str(char *res, char *s1, char *s2);
 
 #endif
